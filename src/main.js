b7ef6c967bd6ee7cbfb533128263b852c0d1bfb2
Ryan McLean <mcleanrs@comcast.net>
2019-03-22 13:19:31 -0700

    update response to make phone number clickable in main.js

--- src/main.js
+++ src/main.js
@@ -3,20 +3,26 @@ import './input.scss';
 import Doctor from './backEnd.js';
 
 $(document).ready(function() {
+  $(".loading").hide();
+  let doctorAPI = new Doctor();
   $(".form").submit(function(event) {
+    $(".info").text('');
+    $(".loading").show();
     event.preventDefault();
     let docName = $(".name").val();
     $(".name").val('');
     console.log(docName);
-    let doctorAPI = new Doctor();
     let promise = doctorAPI.getDoctor(docName);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      for (let i = 0; i < body.data.length; i++){
-        $(".info").append(`From the name you gave us we found this doctor: ${body.data[i].practices[0].name}<br>You can call them at ${body.data[i].practices[0].phones[0].number}<br><br>`);
+      setTimeout(() => {
+        for (let i = 0; i < body.data.length; i++){
+        $(".info").append(`From the name you gave us we found this doctor: ${body.data[i].practices[0].name}<br>You can call them at <a href=tel:${body.data[i].practices[0].phones[0].number}>${body.data[i].practices[0].phones[0].number}</a><br><br>`);
         console.log(body.data);
-      }
+        $(".loading").hide();
+       }
+     },2000);
     })
   });
 });
