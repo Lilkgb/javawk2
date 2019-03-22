4ffeca85bbf0a7af082dfc0ff7574576ccbfa799
Ryan McLean <mcleanrs@comcast.net>
2019-03-22 15:08:33 -0700

    add input field for city and select option for states so search peramiters will be more defined

--- src/main.js
+++ src/main.js
@@ -11,20 +11,27 @@ $(document).ready(function() {
   $(".findDoctor").submit(function(event) {
     $(".info").text('');
     event.preventDefault();
-    let docName = $(".name").val();
+    const state = $("option:selected").val();
+    const cityInput = $(".city").val();
+    const city = cityInput.toLowerCase();
+    console.log(city);
+    const docName = $(".name").val();
     $(".name").val('');
     if (docName == '') {
       return $(".info").text("You did not input correct information. Please try again");
     }
-    $(".loading").show();
-    let promise = doctorAPI.getDoctor(docName);
+    // $(".loading").show();
+    let promise = doctorAPI.getDoctor(docName, city, state);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
       setTimeout(() => {
         console.log(body.data);
         for (let i = 0; i < body.data.length; i++){
-        $(".info").append(`<div class="names"> Doctor:<b> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</b><br>Works at: <b>${body.data[i].practices[0].name}</b><br> Specialty:<b> ${body.data[i].specialties[0].name}</b><br> Phone Number <a href=tel:${body.data[i].practices[0].phones[0].number}>${body.data[i].practices[0].phones[0].number}</a></div>`);
+        $(".info").append(`<div class="names"> Doctor:<b> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</b><br> Gender: <b>${body.data[i].profile.gender}</b><br> Works at: <b>${body.data[i].practices[0].name}</b><br> Specialty:<b> ${body.data[i].specialties[0].name}</b><br> Phone Number <a href=tel:${body.data[i].practices[0].phones[0].number}>${body.data[i].practices[0].phones[0].number}</a></div>`);
+        if(body.data[i].profile.gender == "undefined") {
+          return "Not Listed"
+        }
         $(".loading").hide();
        }
      },2000);
