87e369762b570715dae5da67ca98b9e1f05444bc
Ryan McLean <mcleanrs@comcast.net>
2019-03-22 11:38:50 -0700

    backEnd function working in main.js

--- src/backEnd.js
+++ src/backEnd.js
@@ -1,8 +1,8 @@
-export class Doctor {
+export default class Doctor {
   getDoctor(doctor) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=Ryan&location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=1444ae1df809a2c84653c5b90a9e9726`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -3,5 +3,20 @@ import './input.scss';
 import Doctor from './backEnd.js';
 
 $(document).ready(function() {
-  
+  $(".form").submit(function(event) {
+    event.preventDefault();
+    let docName = $(".name").val();
+    $(".name").val('');
+    console.log(docName);
+    // var xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`);
+    // xhr.done(function(data) { console.log("success got data", data); });
+    let doctorAPI = new Doctor();
+    let promise = doctorAPI.getDoctor(docName);
+    console.log(promise);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $(".info").text(`${body.data.practices.name}`);
+    })
+  });
 });
