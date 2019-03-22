731f82853162632fea715622695053907caa2428
Ryan McLean <mcleanrs@comcast.net>
2019-03-22 12:30:32 -0700

    adjust main.js console log

--- src/main.js
+++ src/main.js
@@ -8,17 +8,14 @@ $(document).ready(function() {
     let docName = $(".name").val();
     $(".name").val('');
     console.log(docName);
-    // var xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`);
-    // xhr.done(function(data) { console.log("success got data", data); });
     let doctorAPI = new Doctor();
     let promise = doctorAPI.getDoctor(docName);
-    console.log(promise);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
       for (let i = 0; i < body.data.length; i++){
         $(".info").append(`From the name you gave us we found this doctor: ${body.data[i].practices[0].name}<br>You can call them at ${body.data[i].practices[0].phones[0].number}<br><br>`);
-        console.log(body.data[i]);
+        console.log(body.data);
       }
     })
   });
