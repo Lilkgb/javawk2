de8c48b910394f53f0faf1576adefac73e95c982
Ryan McLean <mcleanrs@comcast.net>
2019-03-22 13:34:01 -0700

    make basic dom elements to make dom look more presentable

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './input.scss';
 import Doctor from './backEnd.js';
 
 $(document).ready(function() {
-  $(".loading").hide();
   let doctorAPI = new Doctor();
   $(".form").submit(function(event) {
     $(".info").text('');
