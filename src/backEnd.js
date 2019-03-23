aa237ae18e1cf9cfba737ccd833fa373804c781c
Ryan McLean <mcleanrs@comcast.net>
2019-03-22 17:08:50 -0700

    location service is successfully getting lat and lon and putting it into the doctor api and returning correct information

--- src/backEnd.js
+++ src/backEnd.js
@@ -14,12 +14,12 @@ export default class Doctor {
       request.send();
     })
   }
-  getDoctor(docName,location) {
+  getDoctor(docName,lat,lon) {
     // var xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`);
     // xhr.done(function(data) { console.log("success got data", data); });
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=${location}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=1444ae1df809a2c84653c5b90a9e9726`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=${lat}%2C${lon}%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=1444ae1df809a2c84653c5b90a9e9726`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -15,32 +15,34 @@ $(document).ready(function() {
     const state = $("option:selected").val();
     const cityInput = $(".city").val();
     const city = cityInput.toLowerCase();
-    let locationPromise = doctorAPI.getLocation(city, state);
-    locationPromise.then(function(response) {
-      let body = JSON.parse(response);
-      console.log(body);
-    })
     const docName = $(".name").val();
     $(".name").val('');
     if (docName == '') {
       return $(".info").text("You did not input correct information. Please try again");
     }
     // $(".loading").show();
-    let promise = doctorAPI.getDoctor(docName, location);
-    promise.then(function(response) {
+    let locationPromise = doctorAPI.getLocation(city, state);
+    locationPromise.then(function(response) {
       let body = JSON.parse(response);
-      $(".recentlySearched").show();
-      $(".recent").append(`${docName} `)
-      setTimeout(() => {
+      let lat = `${body[0].lat}`;
+      let lon = `${body[0].lon}`;
+      console.log(lat, lon);
+      let promise = doctorAPI.getDoctor(docName, lat, lon);
+      promise.then(function(response) {
+        let body = JSON.parse(response);
         console.log(body);
-        for (let i = 0; i < body.data.length; i++){
-        $(".info").append(`<div class="names"> Doctor:<b> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</b><br> Gender: <b>${body.data[i].profile.gender}</b><br> Works at: <b>${body.data[i].practices[0].name}</b><br> Specialty:<b> ${body.data[i].specialties[0].name}</b><br> Phone Number <a href=tel:${body.data[i].practices[0].phones[0].number}>${body.data[i].practices[0].phones[0].number}</a></div>`);
-        if(body.data[i].profile.gender == "undefined") {
-          return "Not Listed"
-        }
-        $(".loading").hide();
-       }
-     },2000);
+        $(".recentlySearched").show();
+        $(".recent").append(`${docName} `)
+        setTimeout(() => {
+          for (let i = 0; i < body.data.length; i++){
+            $(".info").append(`<div class="names"> Doctor:<b> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</b><br> Gender: <b>${body.data[i].profile.gender}</b><br> Works at: <b>${body.data[i].practices[0].name}</b><br> Specialty:<b> ${body.data[i].specialties[0].name}</b><br> Phone Number <a href=tel:${body.data[i].practices[0].phones[0].number}>${body.data[i].practices[0].phones[0].number}</a></div>`);
+            if(body.data[i].profile.gender == "undefined") {
+              return "Not Listed"
+            }
+            $(".loading").hide();
+          }
+        },2000);
+      })
     })
   });
 });
